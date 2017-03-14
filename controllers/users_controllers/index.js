51955db8f342274897f9020f66aaf3efcdbf00ae
Joe Kitaj <joekitaj@gmail.com>
2017-03-14 17:35:16 -0400

    Change findbyusername to findbyid

--- controllers/users_controllers/index.js
+++ controllers/users_controllers/index.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 
 const controller = require('./controller');
 
-// Route for sign up form
-router.get('/new', controller.new);
+// Route for single user
+router.get('/:id', controller.dashboard);
 
-// Route for loggin form
-router.get('/login', controller.login);
-
-// Route for checking valid login
-router.post('/login', controller.process_login);
-
-// Route for creating a new user
-router.post('/', controller.create);
+// // Route for sign up form
+// router.get('/new', controller.new);
+//
+// // Route for checking valid login
+// router.post('/login', controller.process_login);
+//
+// // Route for creating a new user
+// router.post('/new', controller.create);
 
 module.exports = router;
