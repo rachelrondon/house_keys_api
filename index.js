8470544e17edf0a51dabd82f122f0a26e0691253
Joe Kitaj <joekitaj@gmail.com>
2017-03-15 13:27:45 -0400

    Add cors

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const logger = require('morgan');
 const app = express();
 
+const cors = require('cors');
+app.use(cors());
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
