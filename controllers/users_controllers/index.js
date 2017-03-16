eee489b2ebb503fdcab1989cb9b9210a77462068
Joe Kitaj <joekitaj@gmail.com>
2017-03-16 16:49:04 -0400

    Remove unecessary routes

--- controllers/users_controllers/index.js
+++ controllers/users_controllers/index.js
@@ -2,15 +2,6 @@ const router = require('express').Router();
 
 const controller = require('./controller');
 
-// // Route for login view
-// router.get('/login', controller.login)
-
-// // Route for sign up form
-// router.get('/new', controller.newUser);
-
-// // Route for single user
-// router.get('/:id', controller.dashboard);
-
 // Route for checking valid login
 router.post('/login', controller.processLogin);
 //
