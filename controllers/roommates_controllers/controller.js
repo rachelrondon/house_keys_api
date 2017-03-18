61921b8664777a1710ce2ba794210b2b73839eec
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-03-18 11:46:52 -0400

    test

--- controllers/roommates_controllers/controller.js
+++ controllers/roommates_controllers/controller.js
@@ -1,6 +1,32 @@
 const controller = {};
 const Roommate = require('../../models/roommate');
 
+controller.createRM = (req, res) => {
+  Roommate
+  .createRM(req.body.roommate)
+  .then((roommate) => {
+    res
+    .status(201)
+    .json(roommate);
+  })
+  .catch((err) => {
+    res
+    .status(400)
+    .json(err);
+  });
+};
+
+controller.rmShow = (req, res) => {
+  Roommate
+  .findAll()
+  .then((data) => {
+    res.json(data)
+  })
+  .catch(err => console.log('ERROR:', err));
+}
+
+
+
 controller.male = (req, res) => {
   Roommate
   .findByGender(1)
@@ -73,28 +99,5 @@ controller.rmAge40plus = (req, res) => {
   .catch(err => console.log('ERROR:', err));
 }
 
-controller.createRM = (req, res) => {
-  Roommate
-  .createRM(req.body.roommate)
-  .then((roommate) => {
-    res
-    .status(201)
-    .json(roommate);
-  })
-  .catch((err) => {
-    res
-    .status(400)
-    .json(err);
-  });
-};
-
-controller.rmShow = (req, res) => {
-  Roommate
-  .findAll()
-  .then((data) => {
-    res.json(data)
-  })
-  .catch(err => console.log('ERROR:', err));
-}
 
 module.exports = controller;
