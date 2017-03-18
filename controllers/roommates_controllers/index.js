8b53ad5b04b428703c5bd808e8e9cef32227bf41
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-03-17 22:01:25 -0400

    Test

--- controllers/roommates_controllers/index.js
+++ controllers/roommates_controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const controller = require('./controller');
 
 router.get('/', controller.rmShow);
-router.get('/dashboard', controller.gender);
+router.get('/', controller.gender);
 
-router.get('/dashboard/male', controller.gender);
-router.get('/dashboard/female', controller.gender);
+router.get('/male', controller.gender);
+router.get('/female', controller.gender);
 
 
 router.post('/new', controller.createRM);
